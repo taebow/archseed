e4427dadb277df4aca61cfa5d4ff62954d56ce19
Thibaut Frain <thibaut.frain@silent-waves.com>
2026-01-27 18:10:01 +0100

    It seems to work now

--- qemu-cmd.sh
+++ qemu-cmd.sh
@@ -3,10 +3,10 @@
 DISK_PATH=/tmp/archiso-test.img
 EFI_VARS=/tmp/archiso-test-efi-vars.fd
 
-# rm $DISK_PATH
-# qemu-img create -f qcow2 $DISK_PATH 10G
+rm $DISK_PATH
+qemu-img create -f qcow2 $DISK_PATH 10G
 
-# cp /usr/share/ovmf/x64/OVMF_VARS.4m.fd $EFI_VARS
+cp /usr/share/ovmf/x64/OVMF_VARS.4m.fd $EFI_VARS
 
 qemu-system-x86_64 \
     -enable-kvm \
@@ -15,10 +15,9 @@ qemu-system-x86_64 \
     -smp 4 \
     -drive if=pflash,format=raw,readonly=on,file=/usr/share/ovmf/x64/OVMF_CODE.4m.fd \
     -drive if=pflash,format=raw,file=$EFI_VARS \
-    # -cdrom images/"$(ls -1 images | sort | tail -n 1)" \
-    -drive file=$DISK_PATH,format=qcow2 \
-    # -drive file=$DISK_PATH,if=none,id=nvm,cache=none,aio=native \
-    # -device nvme,serial=deadbeef,drive=nvm \
+    -drive file=$DISK_PATH,if=none,id=nvm,cache=none,aio=native \
+    -cdrom images/"$(ls -1 images | sort | tail -n 1)" \
+    -device nvme,serial=deadbeef,drive=nvm \
     -nic user,model=virtio-net-pci \
     -vga virtio \
     -display sdl,gl=on \
