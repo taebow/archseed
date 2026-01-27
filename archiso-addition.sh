785853a18246f949a3811d3123a7fe55b1a53d73
Thibaut Frain <thibaut.frain@silent-waves.com>
2026-01-27 16:59:01 +0100

    dead end, still problems with passwd, bootctl not creating efi entries, creating entry manually with efibootmgr works, but entry is not read by ovmf

--- archiso-addition.sh
+++ archiso-addition.sh
@@ -11,8 +11,13 @@ PROFILE_PATH=$PROFILE_DIR/$PROFILE_NAME
 cp -Rv /usr/share/archiso/configs/$PROFILE_NAME $PROFILE_DIR/
 mkdir -p $PROFILE_PATH/airootfs/usr/bin
 
-cp ./src/mkpart $PROFILE_PATH/airootfs/usr/bin/
-echo 'file_permissions["/usr/bin/mkpart"]="0:0:755"' >> $PROFILE_PATH/profiledef.sh
+cp ./src/fd-mkpart $PROFILE_PATH/airootfs/usr/bin/
+echo 'file_permissions["/usr/bin/fd-mkpart"]="0:0:755"' >> $PROFILE_PATH/profiledef.sh
+
+
+cp ./src/fd-install $PROFILE_PATH/airootfs/usr/bin/
+echo 'file_permissions["/usr/bin/fd-install"]="0:0:755"' >> $PROFILE_PATH/profiledef.sh
+
 
 echo 'KEYMAP=dvorak' >> $PROFILE_PATH/airootfs/etc/vconsole.conf
 sed -i -e 's/^timeout .*/timeout 0/' -e 's/^beep on/beep off/' $PROFILE_PATH/efiboot/loader/loader.conf
